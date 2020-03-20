96fd15c8d372e09c7eb6a9114c8a740d1f6cf81b
bendaprile <benjamin.daprile@gmail.com>
2020-03-20 19:19:37 -0400

    Create state machine in Text101

---
+++ Text101/Text101/Assets/AdventureGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // [SerializeField] Means that we now have this field available in the inspector     // and we can set the variable from there instead of opening the script.
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+
+        // The () around the text represent it as a string
+        textComponent.text = state.GetStoryText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Text101/Text101/Assets/State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This creates a menu item when you right click on the assets folder named “State”
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+
+    // TextArea makes the text box in Unity much larger [TextArea(minSize, amountOfLinesBeforeScrolling)]
+    [TextArea(18, 10)] [SerializeField] string storyText;
+
+    public string GetStoryText()
+    {
+        return storyText;
+    }
+
+}
