929235b6344f4e5265ee6e6dde8a30b759a5466c
bendaprile <benjamin.daprile@gmail.com>
2020-03-26 18:22:15 -0400

    Construct scenes

---
+++ Number_Wizard_UI/Number Wizard UI/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    private int startingScene = 0;
+
+    public void LoadNextScene()
+    {
+        // Grabs the index of the scene when this method is called
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Loads the scene that has the index one higher than our current one
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        // Loads the first scene in the build
+        SceneManager.LoadScene(startingScene);
+    }
+}
