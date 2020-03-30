e012bc76776fca0c9dfdc083892210c0e508c75e
bendaprile <benjamin.daprile@gmail.com>
2020-03-29 20:05:16 -0400

    Lock ball to paddle and add launch logic

---
+++ Block_Breaker/Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    // Links to the Paddle gameobject
+    [SerializeField] Paddle paddle;
+
+    [SerializeField] float xPushVel = 2f;
+    [SerializeField] float yPushVel = 15f;
+
+    // State Difference Vector (Between paddle and ball)
+    Vector2 paddleToBallVedctor;
+
+    private bool hasLaunched = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Sets the vector to the difference between the ball and the paddle positions
+        paddleToBallVedctor = transform.position - paddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasLaunched == false)
+        {
+            LockBallToPaddle();
+            LaunchBallOnMouseClick();
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        // Vector to track the paddle's current position
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+
+        // Set the balls position to the paddle's + the difference vector
+        transform.position = paddlePos + paddleToBallVedctor;
+    }
+
+    private void LaunchBallOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPushVel, yPushVel);
+            hasLaunched = true;
+        }
+
+    }
+}
--- Block_Breaker/Block Breaker/Assets/Scripts/Paddle.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/Paddle.cs
@@ -6,16 +6,33 @@ public class Paddle : MonoBehaviour
 {
 
     [SerializeField] float screenWidthUnits = 16f;
+    [SerializeField] float minXInUnits = 0f;
+    [SerializeField] float maxXInUnits = 16f;
+    [SerializeField] float paddleSize = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Calculates the min and max relative to the size of the Paddle
+        minXInUnits = minXInUnits + (paddleSize / 2);
+        maxXInUnits = maxXInUnits - (paddleSize / 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.mousePosition.x / Screen.width * screenWidthUnits);
+
+        // Calculates the position of the mouse in Units
+        float xMousePosUnits = Input.mousePosition.x / Screen.width * screenWidthUnits;
+        //Debug.Log(xMousePosUnits.ToString);
+
+        // Creates a Vector2 (x, y) with the current x position and y position of the Paddle
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+
+        // Mathf.Clamp limits the location of the Paddle between the min and max.
+        paddlePos.x = Mathf.Clamp(xMousePosUnits, minXInUnits, maxXInUnits);
+
+        // Move the "Paddle" gameobject to the paddlePos position
+        transform.position = paddlePos;
     }
 }
