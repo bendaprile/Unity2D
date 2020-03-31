77c25001031abfdf1f9b79af9ec05ada08bbc784
bendaprile <benjamin.daprile@gmail.com>
2020-03-31 13:21:47 -0400

    Add level gameobject and script

--- Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
 
+    [SerializeField] AudioClip breakSound;
+
     // This method is called when anything collides with the block
     // The collision parameter will tell us what collided with the block
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+        // Creates a new AudioSource with break sound at the position of the camera
+        // This will play even if the block has been destroyed
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+
         // This destroys the Block and takes it out of the hierarchy
         Destroy(gameObject);
     }
