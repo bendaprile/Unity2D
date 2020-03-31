af38bef2044fe62d2b55f15e230dc228b0ceaf66
bendaprile <benjamin.daprile@gmail.com>
2020-03-31 14:21:00 -0400

    Add difficulty buttons, no functionality

--- Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
@@ -7,14 +7,33 @@ public class Block : MonoBehaviour
 
     [SerializeField] AudioClip breakSound;
 
+    // cached reference
+    Level level;
+
+    private void Start()
+    {
+        // Instantiates level reference to the Level object so we can call public methods
+        level = FindObjectOfType<Level>();
+
+        // Increments the breakableBlocks variable in the Level.cs
+        level.CountBreakableBlocks();
+    }
+
     // This method is called when anything collides with the block
     // The collision parameter will tell us what collided with the block
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBlock();
+    }
 
+    private void DestroyBlock()
+    {
         // Creates a new AudioSource with break sound at the position of the camera
         // This will play even if the block has been destroyed
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+
+        // Decrements the breakableBlocks variable in Level.cs
+        level.BlockDestroyed();
 
         // This destroys the Block and takes it out of the hierarchy
         Destroy(gameObject);
--- Block_Breaker/Block Breaker/Assets/Scripts/Level.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/Level.cs
@@ -6,9 +6,29 @@ public class Level : MonoBehaviour
 {
     [SerializeField] int breakableBlocks; // Serialized for debugging purposes
 
+    // cached reference
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    // Increments the breakableBlocks variable each time the Block script calls this method
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
+    }
+
+    // Decrement breakableBlocks each time the Block script calls this method
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
     }
 
     
