c6bfa0311411c54b4e45b5a96a41b34180e559b1
bendaprile <benjamin.daprile@gmail.com>
2020-04-10 17:46:34 -0400

    Add multiple enemies spawns

--- Laser_Defender/Laser Defender/Assets/Scripts/EnemySpawner.cs
+++ Laser_Defender/Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] List<WaveConfig> waveConfigs;
+
+    int startingWave = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        var maxEnemies = waveConfig.GetNumberOfEnemies();
+
+        // Loop for as many times as number of enemies we want to spawn
+        for (int i=0; i<maxEnemies; i++)
+        {
+            // Instantiate our waveConfig using getters for...
+            // EnemyPrefab, Waypoints.position, and no rotation
+            Instantiate(
+                waveConfig.GetEnemyPrefab(),
+                waveConfig.GetWaypoints()[0].position,
+                Quaternion.identity);
+
+            // Wait a certain amount of time before spawning another enemy
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
     }
 
     // Update is called once per frame
