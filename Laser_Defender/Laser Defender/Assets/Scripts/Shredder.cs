934837aef1c78ff85625b402af73da96a809a049
bendaprile <benjamin.daprile@gmail.com>
2020-04-10 15:01:37 -0400

    Add shredder to destroy lasers

---
+++ Laser_Defender/Laser Defender/Assets/Scripts/Shredder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+{
+
+    // When something passes through the shredder, destroy that GameObject
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
