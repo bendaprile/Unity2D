a36b3a819a522095d90bf0bbce696b13376be482
bendaprile <benjamin.daprile@gmail.com>
2020-04-11 18:31:08 -0400

    Add damage element

--- Laser_Defender/Laser Defender/Assets/Scripts/Enemy.cs
+++ Laser_Defender/Laser Defender/Assets/Scripts/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] float health = 100;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,28 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Grab the DamageDealer script from the other gameobject
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+
+        // Subtracts health and checks if the enemy should be destroyed
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        // Subtract the damage from the heatlh
+        health -= damageDealer.GetDamage();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        // Destroys the laser so it cannot hurt anything else
+        damageDealer.Hit();
     }
 }
