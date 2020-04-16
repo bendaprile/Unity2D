d23cfe6ef2d67a56e6daa8cd04ce4625e2b324ba
bendaprile <benjamin.daprile@gmail.com>
2020-04-16 17:29:27 -0400

    Create Glitch_Garden and start and splash scenes

---
+++ Glitch_Garden/Glitch Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    [SerializeField] string splashScreenName = "Splash Screen";
+    [SerializeField] string startScreenName = "Start Screen";
+
+    private void Awake()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            StartCoroutine(LoadStartSceneAfterDelay());
+        }
+    }
+
+    IEnumerator LoadStartSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(3f);
+
+        LoadStartScene();
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(startScreenName);
+    }
+
+    public void LoadSplashScene()
+    {
+        SceneManager.LoadScene(splashScreenName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
