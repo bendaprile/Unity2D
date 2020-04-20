d70146c54bd6a3eb2b0a2e4b2936f64f3cb9a789
bendaprile <benjamin.daprile@gmail.com>
2020-04-20 18:38:27 -0400

    More animation

--- Glitch_Garden/Glitch Garden/Assets/Scripts/AttackerSpawner.cs
+++ Glitch_Garden/Glitch Garden/Assets/Scripts/AttackerSpawner.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class AttackerSpawner : MonoBehaviour
 {
+    [SerializeField] Attacker attackerPrefab;
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+
+    bool spawn = true;
+
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
+
+        // While spawn is true this will do the SpawnAttacker coroutine
+        // As soon as spawn is false this coroutine will stop
+        while (spawn)
+        {
+            // Wait for a random amount of time between minSpawnDelay and maxSpawnDelay
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+
+        // Spawn an attacker
+        var newAttacker = Instantiate(attackerPrefab, transform.position, Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
