eea2b6c55e3e9813a338f8776d86361cf0de5c47
bendaprile <benjamin.daprile@gmail.com>
2020-04-21 14:18:10 -0400

    Click to spawn enemy

---
+++ Glitch_Garden/Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject defender;
+
+    private void OnMouseDown()
+    {
+        Vector2 mousePos = GetSquareClicked();
+
+        SpawnDefender(mousePos);
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        // Grab the point on the screen that was clicked
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        // Convert that to a world position and return
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+
+        Vector2 gridWorldPos = SnapToGrid(worldPos);
+
+        return gridWorldPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        // Round x and y so they stick to the exact grid cell that was clicked in
+        rawWorldPos.x = Mathf.RoundToInt(rawWorldPos.x);
+        rawWorldPos.y = Mathf.RoundToInt(rawWorldPos.y) + 0.1f;
+
+        return rawWorldPos;
+    }
+
+    private void SpawnDefender(Vector2 mousePos)
+    {
+        GameObject newDefender = Instantiate(defender, mousePos, Quaternion.identity) as GameObject;
+    }
+}
