1e4698d014a6f58f2af4fd56b4912ff62aa9744d
bendaprile <benjamin.daprile@gmail.com>
2020-05-07 16:09:59 -0400

    Level exit

---
+++ TileVania/TileVania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] float levelExitSlowMoveFactor = 0.2f;
+    float currentTimeScale;
+
+    private void Start()
+    {
+        currentTimeScale = Time.timeScale;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>())
+        {
+            StartCoroutine(LoadNextLevel());
+        }
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        Time.timeScale = levelExitSlowMoveFactor; 
+
+        yield return new WaitForSecondsRealtime(2f);
+
+        Time.timeScale = currentTimeScale;
+
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
