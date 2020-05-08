68559f54d01afa2db48659ff6200e6743ac8c6a5
bendaprile <benjamin.daprile@gmail.com>
2020-05-08 09:53:42 -0400

    Add CoinPickup.cs

---
+++ TileVania/TileVania/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+
+    [SerializeField] AudioClip coinPickupSFX;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (coinPickupSFX)
+        {
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
+        }
+        else
+        {
+            Debug.Log("No Coin Pickup SFX Assigned!");
+        }
+        Destroy(gameObject);
+    }
+}
