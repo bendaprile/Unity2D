9a1893fb642aaed0f09abdd1642c23fc73f37271
bendaprile <benjamin.daprile@gmail.com>
2020-05-08 11:38:06 -0400

    Rising water

---
+++ TileVania/TileVania/Assets/Scripts/VeritcalScroll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VeritcalScroll : MonoBehaviour
+{
+
+    [SerializeField] float waterRiseSpeed = 0.3f;
+
+    Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        myRigidbody.velocity = new Vector2(0f, waterRiseSpeed);
+    }
+}
